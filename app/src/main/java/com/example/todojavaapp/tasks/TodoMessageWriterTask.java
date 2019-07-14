02b72ceeed6667511a56d93025cca9db45720ed0
Alex Hart <alex.hart658@gmail.com>
2019-07-14 13:05:03 -0300

    Remove Log and update explanation

--- app/src/main/java/com/example/todojavaapp/tasks/TodoMessageWriterTask.java
+++ app/src/main/java/com/example/todojavaapp/tasks/TodoMessageWriterTask.java
@@ -1,7 +1,6 @@
 package com.example.todojavaapp.tasks;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.example.todojavaapp.data.TodoListDao;
 import com.example.todojavaapp.data.TodoListItem;
@@ -24,8 +23,9 @@ class TodoMessageWriterTask extends AsyncTask<TodoListItem, Void, Void> {
     @Override
     protected Void doInBackground(TodoListItem... todoListItems) {
 
-        // If item has already been sent, we can ignore any additional ones...
-        Log.d("WRITER", "ADD " + todoListItems[0].id);
+        // If item has already been sent, we can ignore any additional passed items.
+        // Passed item beyond first is only a "new line".  This piece of code basically
+        // makes sure we don't end up with a race condition.
         if (viewedIds.contains(todoListItems[0].id)) {
             todoListDao.addTodoListItem(todoListItems[0]);
             return null;
